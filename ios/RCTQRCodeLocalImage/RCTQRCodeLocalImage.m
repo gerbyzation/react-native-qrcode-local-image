140161c05b75fc8bcdabe536a0388b1010afefdf
Gerben Neven <gerbyzation@me.com>
2018-09-06 11:34:22 +0200

    add support for asset urls

--- ios/RCTQRCodeLocalImage/RCTQRCodeLocalImage.m
+++ ios/RCTQRCodeLocalImage/RCTQRCodeLocalImage.m
@@ -10,44 +10,52 @@
 #import <React/RCTLog.h>
 #import <React/RCTUtils.h>
 #import "RCTQRCodeLocalImage.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @implementation RCTQRCodeLocalImage
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(decode:(NSString *)path callback:(RCTResponseSenderBlock)callback)
 {
-    UIImage *srcImage;
-    if ([path hasPrefix:@"http://"] || [path hasPrefix:@"https://"]) {
-        srcImage = [UIImage imageWithData: [NSData dataWithContentsOfURL:[NSURL URLWithString: path]]];
-    } else {
-        srcImage = [[UIImage alloc] initWithContentsOfFile:path];
-    }
-    if (nil==srcImage){
-        NSLog(@"PROBLEM! IMAGE NOT LOADED\n");
-        callback(@[RCTMakeError(@"IMAGE NOT LOADED!", nil, nil)]);
-        return;
-    }
-    NSLog(@"OK - IMAGE LOADED\n");
-    NSDictionary *detectorOptions = @{@"CIDetectorAccuracy": @"CIDetectorAccuracyHigh"};
-    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode context:nil options:detectorOptions];
-    CIImage *image = [CIImage imageWithCGImage:srcImage.CGImage];
-    NSArray *features = [detector featuresInImage:image];
-    if (0==features.count) {
-        NSLog(@"PROBLEM! Feature size is zero!\n");
-        callback(@[RCTMakeError(@"Feature size is zero!", nil, nil)]);
-        return;
-    }
-    
-    CIQRCodeFeature *feature = [features firstObject];
-    
-    NSString *result = feature.messageString;
-    NSLog(@"result: %@", result);
-    
-    if (result) {
-        callback(@[[NSNull null], result]);
-    } else {
+    __block UIImage *srcImage;
+    ALAssetsLibrary *assetLibrary=[[ALAssetsLibrary alloc] init];
+    [assetLibrary assetForURL:[NSURL URLWithString: path] resultBlock:^(ALAsset *asset) {
+        ALAssetRepresentation *rep = [asset defaultRepresentation];
+        Byte *buffer = (Byte*)malloc(rep.size);
+        NSUInteger buffered = [rep getBytes:buffer fromOffset:0.0 length:rep.size error:nil];
+        NSData *data = [NSData dataWithBytesNoCopy:buffer length:buffered freeWhenDone:YES];
+        srcImage = [UIImage imageWithData: data];
+        
+        if (nil==srcImage){
+            NSLog(@"PROBLEM! IMAGE NOT LOADED\n");
+            callback(@[RCTMakeError(@"IMAGE NOT LOADED!", nil, nil)]);
+            return;
+        }
+        NSLog(@"OK - IMAGE LOADED\n");
+        NSDictionary *detectorOptions = @{@"CIDetectorAccuracy": @"CIDetectorAccuracyHigh"};
+        CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode context:nil options:detectorOptions];
+        CIImage *image = [CIImage imageWithCGImage:srcImage.CGImage];
+        NSArray *features = [detector featuresInImage:image];
+        if (0==features.count) {
+            NSLog(@"PROBLEM! Feature size is zero!\n");
+            callback(@[RCTMakeError(@"Feature size is zero!", nil, nil)]);
+            return;
+        }
+        
+        CIQRCodeFeature *feature = [features firstObject];
+        
+        NSString *result = feature.messageString;
+        NSLog(@"result: %@", result);
+        
+        if (result) {
+            callback(@[[NSNull null], result]);
+        } else {
+            callback(@[RCTMakeError(@"QR Parse failed!", nil, nil)]);
+            return;
+        }
+    } failureBlock:^(NSError *error) {
         callback(@[RCTMakeError(@"QR Parse failed!", nil, nil)]);
         return;
-    }
+    }] ;
 }
 @end
